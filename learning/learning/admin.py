734272e8a4a76702538b41b98f7c08959064d61e
Jiangjane543 <81281462+Jiangjane543@users.noreply.github.com>
2021-03-31 15:44:33 +0800

    Delete learning/learning directory

--- learning/learning/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from learning.models import Topic,Entry
-# Register your models here.
-admin.site.register(Topic) #通过管理网站管理
-admin.site.register(Entry)
--- learning/learning/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Entry
-
-class EntryForm(forms.ModelForm):
-    class Meta:
-        model=Entry
-        fields=['text']
-        labels={'text':''}
-        widgets={'text':forms.Textarea(attrs={'cols':80})}#widget小部件，可设置属性更改部件类型，如单行文本框、多行、下拉列表
